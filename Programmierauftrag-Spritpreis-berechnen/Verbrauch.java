332ec6f43c183b7870bc2391dd5cea231b4f5d82
Florian Fink <flo.fink.124@web.de>
2020-01-21 11:21:43 +0100

    Einrueckungsfehler

--- Programmierauftrag-Spritpreis-berechnen/Verbrauch.java
+++ Programmierauftrag-Spritpreis-berechnen/Verbrauch.java
@@ -27,23 +27,23 @@ public class Verbrauch {
   }
   public double streckeMit1Liter() {
     return 1 / (verbrauchPro100Km() / 100);
- }
- public double spezifischeCO2Emission(String spritArt) {
-   if (spritArt.equalsIgnoreCase("diesel")) {
-     return 2.62 * (this.spritMenge / this.strecke);
-   } else if (spritArt.equalsIgnoreCase("benzin")) {
-     return 2.32 * (this.spritMenge / this.strecke);
-   } else {
-     return 0.0;
-   }
   }
- public double absoluteCO2Emission(String spritArt) {
-  if (spritArt.equalsIgnoreCase("diesel")) {
-    return (this.spezifischeCO2Emission(spritArt) * this.strecke) / 1000;
-  } else if (spritArt.equalsIgnoreCase("benzin")) {
-    return (this.spezifischeCO2Emission(spritArt) * this.strecke) / 1000;
-  } else {
-    return 0.0;
+  public double spezifischeCO2Emission(String spritArt) {
+    if (spritArt.equalsIgnoreCase("diesel")) {
+      return 2.62 * (this.spritMenge / this.strecke);
+    } else if (spritArt.equalsIgnoreCase("benzin")) {
+      return 2.32 * (this.spritMenge / this.strecke);
+    } else {
+      return 0.0;
+    }
   }
- }
+  public double absoluteCO2Emission(String spritArt) {
+    if (spritArt.equalsIgnoreCase("diesel")) {
+      return (this.spezifischeCO2Emission(spritArt) * this.strecke) / 1000;
+    } else if (spritArt.equalsIgnoreCase("benzin")) {
+      return (this.spezifischeCO2Emission(spritArt) * this.strecke) / 1000;
+    } else {
+      return 0.0;
+    }
+  }
 }
