de2f770d6d72fb6f941a611bf15ab0d619e2c51f
Florian Fink <flo.fink.124@web.de>
2020-05-12 11:15:13 +0200

    Vererbungs Aufgaben mit Test

---
+++ Vererbung/Viereck.java
@@ -0,0 +1,18 @@
+import java.lang.Math;
+
+public class Viereck {
+
+  static double breite;
+  static double laenge;
+
+  // Methods
+  double flaecheBerechnen() {
+    return breite * laenge;
+  }
+
+  double umfangBerechnen() {
+    return 2 * breite + 2 * laenge;
+  }
+
+
+}
